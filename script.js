0b0e56e383a4ae3965f1eb8a0f978e027831170b
yurilopesm <yurilopesm@hotmail.com>
2023-01-18 09:52:02 -0300

    versao com menu-lateral

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
-function codar() {
-
+function alteraMenu() {
+    const menuLateral = document.querySelector('.menu-lateral');
+    const fundo = document.querySelector('.fundo-tela');
+    menuLateral.classList.toggle('esconder-lateral');
+    fundo.classList.toggle('esconder-fundo');
 }
