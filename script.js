40240d55d6fead82494d01b3bf2f50adaa144630
yurilopesm <yurilopesm@hotmail.com>
2023-01-23 10:37:00 -0300

    versao final com o bonus

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-let urlApi = "https://mock-api.driven.com.br/api/v6/uol/"
 let nome;
+let body;
 let ultimaMensagem;
+let destinatario = "Todos";
+let typeMessage = "message";
 function alteraMenu() {
     const menuLateral = document.querySelector('.menu-lateral');
     const fundo = document.querySelector('.fundo-tela');
     menuLateral.classList.toggle('esconder-lateral');
     fundo.classList.toggle('esconder-fundo');
 }
-function logIn() {
+function marcarCheck(li, contato) {
+    let selectedAnt = document.querySelector('.selecionado');
+    selectedAnt.classList.remove('selecionado');
+    li.classList.add('selecionado');
+    destinatario = contato;
     
-    nome = {
-        name: `"${document.querySelector('.input-login').value}"`
+}
+function marcarVisibilidade(visibility) {
+    typeMessage = "message";
+    let selecionadoAnteriormente = document.querySelector('.selected');
+    if(selecionadoAnteriormente !== null) {
+        selecionadoAnteriormente.classList.remove('selected');
+    }
+    visibility.classList.add('selected');
+    if(visibility.classList.contains("visibilidade-reservado")) {
+        typeMessage = "private_message";
+    }
+   
+}
+function logIn() {
+    nome = document.querySelector('.input-login').value;
+    body = {
+        name: nome
       }
-      
-    const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants ", nome);
+    if(document.querySelector('.input-login').value === "") {
+        alert("Por favor escolha um nome!");
+        window.location.reload();
+    } else {
+        const requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", body);
     requisicao.then(logar);
     requisicao.catch(nomeInvalido);
-    
+    } 
 }
 function logar(dado) {
     const telaLogin = document.querySelector('.tela-login');
     telaLogin.classList.add('esconder-fundo');
+    loadMessages();
 }
 function nomeInvalido(erro) {
     if(erro.response.status == "400"){
@@ -44,7 +69,7 @@ function renderMessages(answer) {
             ulMessage.innerHTML += `<li data-test="message" class="status" >${mensagensTela[i].time} <strong>${mensagensTela[i].from}</strong>  ${mensagensTela[i].text}</li>`;
         } else if(mensagensTela[i].type === "message") {
             ulMessage.innerHTML += `<li data-test="message" class="msgPublica" >${mensagensTela[i].time} <strong>${mensagensTela[i].from}</strong> para <strong>${mensagensTela[i].to}</strong>: ${mensagensTela[i].text}`
-        } else if(mensagensTela[i] === "private_message" && mensagensTela[i].to === nome) {
+        } else if(mensagensTela[i].type === "private_message" && (mensagensTela[i].to === nome || mensagensTela[i].from === nome)) {
             ulMessage.innerHTML += `<li data-test="message" class="msgPrivada" >${mensagensTela[i].time} <strong>${mensagensTela[i].from}</strong> reservadamente para <strong>${mensagensTela[i].to}</strong>: ${mensagensTela[i].text}`
         }
     }
@@ -63,7 +88,7 @@ function renderPeople(variavel) {
     const contato = document.querySelector('.contatos');
     for(i = 0; i < variavel.data.length; i++) {
         const peopleTemplate = ` 
-        <li data-test="participant" onclick="marcarCheck()" class="visibilidade-publico">
+        <li data-test="participant" onclick="marcarCheck(this, '${variavel.data[i].name}')" class="visibilidade-publico">
             <ion-icon name="person-circle"></ion-icon>
             ${variavel.data[i].name}
             <ion-icon data-test="check" class="check" name="checkmark-outline"></ion-icon>
@@ -71,9 +96,32 @@ function renderPeople(variavel) {
         contato.innerHTML += peopleTemplate;
     }
 }
+function enviarMensagem() {
+    let inputValue = document.querySelector('.footer input').value;
+    let message = {
+        from: nome,
+        to: destinatario,
+        text: inputValue,
+        type: typeMessage
+    };
+    console.log(message);
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages', message);
+    document.querySelector('.footer input').value = "";
+    promise.then(enviou);
+    promise.catch(naoEnviou);
+
+}
+function enviou() {
+    console.log("enviou");
+
+    loadMessages();
+}
+function naoEnviou(erro) {
+    console.log("nao enviou");
+}
 
 function stayLoad() {
-    let status = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", nome);
+    let status = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", body);
 }
 
 function loadChat() {
@@ -83,3 +131,4 @@ function loadChat() {
     setInterval(loadMessages, 3000);
     setInterval(stayLoad, 5000);
 }
+
